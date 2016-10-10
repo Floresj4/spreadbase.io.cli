1e02df5081f42f6b0a9bc7100af0a828cf14361b
Jason <floresj4@gmail.com>
2016-10-10 19:34:18 -0400

    adding and starting initial rest request, style changes

--- server.js
+++ server.js
@@ -2,13 +2,25 @@
 
 var express = require("express");
 var app = express();
+var request = require('request');
 var path = require('path');
 
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/style'));
 
 app.get("/", function(req, res){
-	console.log("Get /");
+	console.log("Get: /");
+	
+	//get the application version information
+	request('http://127.0.0.1:8080/info', function(err, body){
+		var version = JSON.parse(body.body);
+	});
+	
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/', function(req, res) {
+	console.log("Post: /");
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
