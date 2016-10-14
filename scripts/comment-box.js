99ee34752e3d27dce6d26d6beed66e5f47997bf7
Jason <floresj4@gmail.com>
2016-10-13 22:53:57 -0400

    switch/stretch node logo and build comments section

--- scripts/comment-box.js
+++ scripts/comment-box.js
@@ -3,11 +3,51 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+		
+		<CommentList />
+		<CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+		<Comment author="Lennox Zion">Good stuff!</Comment>
+        <Comment author="Marshmellow">Keep it mello...</Comment>
+      </div>
+    );
+  }
+});
+
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className='comment'>
+				<h2 className='commentAuthor'>
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
+
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
