7e7f8e08a1b342499e808cf0959f7cdc6c989d25
Jason <floresj4@gmail.com>
2016-10-13 20:19:10 -0400

    starting React...

--- server.js
+++ server.js
@@ -7,23 +7,28 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/style'));
+app.use(express.static(__dirname + '/scripts'));
+app.use(express.static(__dirname + '/view'));
 
 app.get("/", function(req, res){
-	console.log("Get: /");
+	console.log("get: /");
+	
 	
 	//get the application version information
-	request('http://127.0.0.1:8080/info', function(err, body){
-		var version = JSON.parse(body.body);
-	});
+//	request('http://127.0.0.1:8080/info', function(err, body){
+//		app.locals.version = JSON.parse(body.body);
+//	});
 	
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/', function(req, res) {
-	console.log("Post: /");
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
+//app.post('/', function(req, res) {
+//	console.log("post: /");
+//	res.sendFile(path.join(__dirname + '/index.html'));
+//});
 
 app.listen(8000, function() {
 	console.log("listenining on port " + 8000);
 });
+
+exports.app = app;
