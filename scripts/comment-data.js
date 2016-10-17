50a1f00ee1cdfa4d089990e74b42d5cbea07c519
Jason <floresj4@gmail.com>
2016-10-16 22:50:55 -0400

    adding a few more lines

--- scripts/comment-data.js
+++ scripts/comment-data.js
@@ -1,4 +1,10 @@
 [
 	{"id": 1, "author": "Lennox Zion", "text": "Good stuff!"},
-	{"id": 2, "author": "Marshmellow", "text": "Keep it mello..."}
+	{"id": 2, "author": "Marshmellow", "text": "Keep it mello..."},
+	{"id": 3, "author": "Diplo", "text": "Revolution"},
+	{
+		"id": 4, 
+		"author": "Ipsum Lorem", 
+		"text": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium "
+	}
 ]
