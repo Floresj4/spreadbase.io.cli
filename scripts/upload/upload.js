c21493c52b947ab33d49fdde9338efaa550bf6c1
Jason <floresj4@gmail.com>
2016-10-20 19:53:40 -0400

    completed simple post

--- scripts/upload/upload.js
+++ scripts/upload/upload.js
@@ -1,14 +1,80 @@
+"use strict";
+
 var UploadForm = React.createClass({
+	getInitialState: function() {
+		return ({data: []});
+	},
+	
+	handleFileChange: function(e) {
+		var file = e.target.files[0];
+		
+		var fd = new FormData();
+		fd.append('file', file, file.name);
+
+		$.ajax({
+			url: 'http://localhost:8080/upload/',
+			type: 'POST',
+			data: fd,
+			processData: false, //don't process the files
+			
+			/**
+			 * set content type to false of jQuery will 
+			 * default to application/x-www-form-urlencoded
+			 */
+			contentType: false, 
+			success: function(data, status, xhr) {
+				console.log('done');
+			},
+			error: function(xhr, status, err) {
+				console.log(xhr.toString() + ' ' + status + ' ' + err);
+			}
+		});
+	},
+
+	handleFormUpload: function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+
+//		//build form data for submission
+//		var data = new FormData();
+//		$.each(this.state.data, function(key, value) {
+//			data.append(key, value);
+//		});
+//		
+//		$.ajax({
+//			url: 'http://localhost:8080/upload/',
+//			type: 'POST',
+//			data: data,
+//			processData: false, //don't process the files
+//			
+//			/**
+//			 * set content type to false of jQuery will 
+//			 * default to application/x-www-form-urlencoded
+//			 */
+//			contentType: false, 
+//			success: function(data, status, xhr) {
+//				console.log('done');
+//			},
+//			error: function(xhr, status, err) {
+//				console.log(xhr.toString() + ' ' + status + ' ' + err);
+//			}
+//		});
+	},
 	
 	render: function() {
 		return (
-			<form method='post' action='/'>
-				<input id='fileUpload' className='btn btn-default' type='file' placeholder='Upload a file' />
+			<form onSubmit={this.handleFormUpload} encType='multipart/form-data'>
+				<input id='fileUpload' 
+					className='btn btn-default' 
+					type='file' 
+					placeholder='Upload a file' 
+					onChange={this.handleFileChange} />
+
 				<input id='submit'
-					   className='btn btn-default' 
-					   type='Submit' 
-					   accept='application/*'
-					   defaultValue='Submit' />
+					className='btn btn-default' 
+					type='Submit' 
+					accept='application/*'
+					defaultValue='Submit' />
 			</form>
 		);
 	}
--- server.js
+++ server.js
@@ -12,21 +12,13 @@ app.use(express.static(__dirname + '/scripts/upload'));
 app.use(express.static(__dirname + '/view'));
 
 app.get("/", function(req, res){
-	console.log("get: /");
-	
-	
-	//get the application version information
-//	request('http://127.0.0.1:8080/info', function(err, body){
-//		app.locals.version = JSON.parse(body.body);
-//	});
-	
+	console.log('get /');
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-//app.post('/', function(req, res) {
-//	console.log("post: /");
-//	res.sendFile(path.join(__dirname + '/index.html'));
-//});
+app.post("/", function(req, res){
+	console.log('post /');
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(8000, function() {
 	console.log("listenining on port " + 8000);
