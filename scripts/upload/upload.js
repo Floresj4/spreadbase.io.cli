86bab029aec2bd181d7061ead6c6fc0cb2be9bb9
Jason <floresj4@gmail.com>
2016-10-20 21:03:58 -0400

    style changes: removing submit button

--- scripts/upload/upload.js
+++ scripts/upload/upload.js
@@ -30,51 +30,16 @@ var UploadForm = React.createClass({
 			}
 		});
 	},
-
-	handleFormUpload: function(event) {
-		event.stopPropagation();
-		event.preventDefault();
-
-//		//build form data for submission
-//		var data = new FormData();
-//		$.each(this.state.data, function(key, value) {
-//			data.append(key, value);
-//		});
-//		
-//		$.ajax({
-//			url: 'http://localhost:8080/upload/',
-//			type: 'POST',
-//			data: data,
-//			processData: false, //don't process the files
-//			
-//			/**
-//			 * set content type to false of jQuery will 
-//			 * default to application/x-www-form-urlencoded
-//			 */
-//			contentType: false, 
-//			success: function(data, status, xhr) {
-//				console.log('done');
-//			},
-//			error: function(xhr, status, err) {
-//				console.log(xhr.toString() + ' ' + status + ' ' + err);
-//			}
-//		});
-	},
 	
 	render: function() {
 		return (
-			<form onSubmit={this.handleFormUpload} encType='multipart/form-data'>
-				<input id='fileUpload' 
-					className='btn btn-default' 
-					type='file' 
-					placeholder='Upload a file' 
-					onChange={this.handleFileChange} />
-
-				<input id='submit'
-					className='btn btn-default' 
-					type='Submit' 
-					accept='application/*'
-					defaultValue='Submit' />
+			<form encType='multipart/form-data'>
+				<div className='form-group'>
+					<input id='fileUpload' 
+						className='btn btn-default form-control' 
+						type='file' 
+						onChange={this.handleFileChange} />
+				</div>
 			</form>
 		);
 	}
